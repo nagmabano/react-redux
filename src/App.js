62ec22dac529c7a79df506050a9f3364ba01a9bd
nagma <bano.nagma.1@gmail.com>
2025-12-13 21:49:43 +0530

    feat: implement authentication flow with login/logout functionality and user profile display

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { Fragment } from 'react/jsx-runtime';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+     {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </Fragment>
   );
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Auth.module.css';
+import { authActions } from '../store/index';
 
 const Auth = () => {
+
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const loginHandler = () => {
+    dispatch(authActions.login());
+  }
+
   return (
     <main className={classes.auth}>
       <section>
@@ -13,7 +23,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={loginHandler}>Login</button>
         </form>
       </section>
     </main>
